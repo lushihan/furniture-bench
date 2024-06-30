d33956562869a45ecd899956a312330ad1344bd4
pandaclient <lushihan287@gmail.com>
2024-06-29 23:59:21 -0700

    minor changes, ignore

--- furniture_bench/scripts/convert_furniture_to_robomimic.py
+++ furniture_bench/scripts/convert_furniture_to_robomimic.py
@@ -113,14 +113,16 @@ def main():
             active_acous_spec_raw = np.array(obs["active_acous_spec"])
             # active_acous_spec_raw_log = 10 * np.log10(active_acous_spec_raw + 1e-10)
             # active_acous_spec_raw_log_normalized = ( active_acous_spec_raw_log - (-100) ) / ( -50 - (-100) )
-            active_acous_spec_raw_linear_normalized = active_acous_spec_raw * 1e7
+            active_acous_spec_raw_linear_normalized = active_acous_spec_raw * 1e5
 
 
             # obs["active_acous_spec"] = active_acous_spec_raw_log_normalized[:, 28:233]
             # obs["active_acous_spec"] = active_acous_spec_raw_log_normalized[:, 70:233:4]
             # obs["active_acous_spec"] = active_acous_spec_raw_log_normalized[:, 70:233]
 
-            obs["active_acous_spec"] = active_acous_spec_raw_linear_normalized[:, 70:233]
+            # obs["active_acous_spec"] = active_acous_spec_raw_linear_normalized[:, 70:233]
+            obs["active_acous_spec"] = active_acous_spec_raw_linear_normalized[:, 70:233:4]
+
 
 
             # print(np.shape(obs["active_acous_spec"]))
