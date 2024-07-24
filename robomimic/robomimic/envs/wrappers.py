24d52668d731f8dea56eafdb964230384ae3245f
pandaclient <lushihan287@gmail.com>
2024-07-24 03:52:09 -0700

    add tactile_rgb modality in obs_utils and wrappers

--- robomimic/robomimic/envs/wrappers.py
+++ robomimic/robomimic/envs/wrappers.py
@@ -268,7 +268,7 @@ class FurniturePreprocessWrapper(EnvWrapper):
         ob_dict["tactile_image"] = ob_dict["tactile_image"]
 
         for k in ob_dict:
-            if (k in ObsUtils.OBS_KEYS_TO_MODALITIES) and (ObsUtils.key_is_obs_modality(key=k, obs_modality="rgb") or ObsUtils.key_is_obs_modality(key=k, obs_modality="depth") or ObsUtils.key_is_obs_modality(key=k, obs_modality="scan")): #??
+            if (k in ObsUtils.OBS_KEYS_TO_MODALITIES) and (ObsUtils.key_is_obs_modality(key=k, obs_modality="rgb") or ObsUtils.key_is_obs_modality(key=k, obs_modality="depth") or ObsUtils.key_is_obs_modality(key=k, obs_modality="scan") or ObsUtils.key_is_obs_modality(key=k, obs_modality="tactile_rgb")): #??
                 ob_dict[k] = ObsUtils.process_obs(obs=ob_dict[k], obs_key=k)
         return ob_dict
 
