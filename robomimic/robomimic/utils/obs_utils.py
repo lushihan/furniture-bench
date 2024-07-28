b871f1b9106d2cd0e160233eb02c0650ea14636b
pandaclient <lushihan287@gmail.com>
2024-07-28 01:28:11 -0700

    change force_array from np.uint16 to np.int16

--- robomimic/robomimic/utils/obs_utils.py
+++ robomimic/robomimic/utils/obs_utils.py
@@ -1002,6 +1002,39 @@ class LowDimModality(Modality):
         return obs
 
 
+class TactileImageModality(Modality):
+    """
+    Modality for tactile image observations
+    """
+    name = "tactile_rgb"
+
+    @classmethod
+    def _default_obs_processor(cls, obs):
+        """
+        Given image fetched from dataset, process for network input. Converts array
+        to float (from uint8), normalizes pixels from range [0, 255] to [0, 1], and channel swaps
+        from (H, W, C) to (C, H, W).
+        Args:
+            obs (np.array or torch.Tensor): image array
+        Returns:
+            processed_obs (np.array or torch.Tensor): processed image
+        """
+        return process_frame(frame=obs, channel_dim=3, scale=255.)
+
+    @classmethod
+    def _default_obs_unprocessor(cls, obs):
+        """
+        Given image prepared for network input, prepare for saving to dataset.
+        Inverse of @process_frame.
+        Args:
+            obs (np.array or torch.Tensor): image array
+        Returns:
+            unprocessed_obs (np.array or torch.Tensor): image passed through
+                inverse operation of @process_frame
+        """
+        return TU.to_uint8(unprocess_frame(frame=obs, channel_dim=3, scale=255.))
+
+
 class ForceArrayModality(Modality):
     """
     Modality for force array observations
